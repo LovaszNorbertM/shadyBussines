274173a80a7af6cd689d1faa000aa73f50fadd67
LovaszNorbertM <lovasz.norbert.m@gmail.com>
2023-01-07 16:37:49 +0200

    shadier business

--- server.js
+++ server.js
@@ -1,39 +1,54 @@
-var http = require('http'); // 1 - Import Node.js core module
+// var http = require('http'); // 1 - Import Node.js core module
 
-var http = require('http'); // Import Node.js core module
+// var http = require('http'); // Import Node.js core module
 
-var server = http.createServer(function (req, res) {   //create web server
-    if (req.url == '/') { //check the URL of the current request
-        const clientIP = req.headers['x-forwarded-for']?.split(',').shift() || req.socket?.remoteAddress;
+// var server = http.createServer(function (req, res) {   //create web server
+//     if (req.url == '/') { //check the URL of the current request
+//         const clientIP = req.headers['x-forwarded-for']?.split(',').shift() || req.socket?.remoteAddress;
     
         
-        // set response header
-        res.writeHead(200, { 'Content-Type': 'text/html' }); 
+//         // set response header
+//         res.writeHead(200, { 'Content-Type': 'text/html' }); 
         
-        // set response content    
-        res.write('<html><body><p>This is home Page.</p></body></html>');
-        res.end(`The client's IP Address is: ${clientIP}`);
+//         // set response content    
+//         res.write('<html><body><p>This is home Page.</p></body></html>');
+//         res.end(`The client's IP Address is: ${clientIP}`);
     
-    }
-    else if (req.url == "/student") {
+//     }
+//     else if (req.url == "/student") {
         
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is student Page.</p></body></html>');
-        res.end();
+//         res.writeHead(200, { 'Content-Type': 'text/html' });
+//         res.write('<html><body><p>This is student Page.</p></body></html>');
+//         res.end();
     
-    }
-    else if (req.url == "/admin") {
+//     }
+//     else if (req.url == "/admin") {
         
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is admin Page.</p></body></html>');
-        res.end();
+//         res.writeHead(200, { 'Content-Type': 'text/html' });
+//         res.write('<html><body><p>This is admin Page.</p></body></html>');
+//         res.end();
     
-    }
-    else
-        res.end('Invalid Request!');
+//     }
+//     else
+//         res.end('Invalid Request!');
 
-});
+// });
 
-server.listen(5000); //6 - listen for any incoming requests
+// server.listen(5000); //6 - listen for any incoming requests
 
-console.log('Node.js web server at port 5000 is running..')
+// console.log('Node.js web server at port 5000 is running..')
+
+
+var express = require('express');
+
+var app = express();
+
+app.get("/",function(req,res){
+    res.end("Your IP address is " + req.ip);
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port number" + PORT);
+})
